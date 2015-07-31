f03169b11ec0b83df19a1b728aff99fd79aff8d6
Prashant <prashantkumar10011989@gmail.com>
2015-07-30 21:31:10 -0600

    deelting redundant files

--- myAMPL/src/Mainfile.hs
+++ myAMPL/src/Mainfile.hs
@@ -28,21 +28,8 @@ main = do
    case conctree of
       Bad msg   -> putStrLn msg
       Ok  tree  -> do
-              putStrLn (show compiled)
+              putStrLn (show tree)
               --ans <- run_cm' compiled
               --putStrLn (show (ans::S.MACH))
-                  where compiled = (compile_all tree)
+               --   where compiled = (compile_all tree)
 
-{-
- Main (Handles [Hand_spec (UIdent "StrTerm") [HandName (UIdent "Get"),HandName (UIdent "Put"),
- HandName (UIdent "Close")]]) (Cohandles [Hand_spec (UIdent "Console")
- [HandName (UIdent "Get"),HandName (UIdent "Put"),HandName (UIdent "Close")]])
-  Constructors_none Destructors_none Processes_none Functions_none (Start (Channel_specf [Ident "console"]
-   [Ident "strTerm1"]) (Prog [AC_HPUTf (Ident "strTerm1") (UIdent "StrTerm") (UIdent "Get"),
-   AC_GETf (Ident "strTerm1"),AC_STOREf (Ident "x"),AC_HPUTf (Ident "strTerm1") (UIdent "StrTerm") (UIdent "Get")
-   ,AC_GETf (Ident "strTerm1"),AC_STOREf (Ident "y"),AC_LOADf (Ident "y"),AC_LOADf (Ident "x"),AC_CONCAT,
-   AC_HPUTf (Ident "strTerm1") (UIdent "StrTerm") (UIdent "Put"),AC_PUTf (Ident "strTerm1"),
-   AC_HPUTf (Ident "strTerm1") (UIdent "StrTerm") (UIdent "Put"),AC_PUTf (Ident "strTerm1"),
-   AC_HPUTf (Ident "strTerm1") (UIdent "StrTerm") (UIdent "Close"),AC_CLOSEf (Ident "strTerm1"),
-   AC_HPUTf (Ident "console") (UIdent "Console") (UIdent "Close"),AC_HALTf (Ident "console")]))   
--}
